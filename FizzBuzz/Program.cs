897457fc9367155b6652d2e51f45db2f0f999549
ZhaQuon Poindexter <zpoindexter@tql.com>
2021-05-19 16:33:08 -0400

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var nbr = 1; nbr <= 30; nbr++)
+            {
+                if(nbr % 3 != 0 && nbr % 5 != 0)
+                {
+                    Console.WriteLine($"{nbr} produced neither");
+                }
+                var deter = nbr % 3 == 0 ? "FIZZ" : "BUZZ";
+                Console.WriteLine($"{nbr} produced {deter}");
+            }
+        }
+    }
+}
